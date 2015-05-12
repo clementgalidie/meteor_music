8181fe799e2c592c881cdf67d86fad00767469e9
Guillaume <guillaume.azzopardi@free.fr>
2015-05-12 19:10:32 +0200

    yahahaha
    Il manque l'upload de la photo, et la verification des formulaires

--- meteorapp/client/js/editStudent.js
+++ meteorapp/client/js/editStudent.js
@@ -1,19 +1,29 @@
-Template.newStudent.events({
-    "submit form": function (event) {
+Template.editstudent.events({
+    "submit .edit-student": function (event) {
         var firstname = event.target.firstname.value;
         var lastname = event.target.lastname.value;
         var mail = event.target.mail.value;
         var dob = event.target.dob.value;
         var pp = event.target.pp.value;
-        var note = event.target.note.value;
+        var notes = event.target.notes.value;
+        var id = event.target.id.value;
+		
 
-        student.insert({
+        student.update({
+			_id: id
+		},					   
+		{
+					   
             firstname: firstname,
             lastname: lastname,
             mail: mail,
             dob: dob,
             pp: pp,
-            note: note
+            notes: notes
         });
+        
+        
+        
+        
     }
-});
+}); 
