b195754c80f6aece8370d6d3b25a2f914595b7a7
Clément Gendry <clement.gendry@hetic.net>
2015-05-12 14:44:16 +0200

    yéhéhé

--- meteorapp/client/js/addStudent.js
+++ meteorapp/client/js/addStudent.js
@@ -1,17 +1,15 @@
-Template.students.events({
+Template.newStudent.events({
     "submit form": function (event) {
         var firstname = event.target.firstname.value;
         var lastname = event.target.lastname.value;
         var mail = event.target.mail.value;
         var dob = event.target.dob.value;
-        var pp = event.target.pp.value;
 
         student.insert({
             firstname: firstname,
             lastname: lastname,
             mail: mail,
-            dob: dob,
-            pp: pp
+            dob: dob
         });
     }
 });
